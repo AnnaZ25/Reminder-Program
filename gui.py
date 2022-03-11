106b1e425f17b7be277a95b9af54ad73fc57d9e4
AnnaZ25 <96466596+AnnaZ25@users.noreply.github.com>
2022-03-11 18:20:53 +0300

    First GUI Attempt
    Things to fix - the <--- button remains on the home page even if used - fix this

---
+++ gui.py
@@ -0,0 +1,73 @@
+#importing the needed module
+from tkinter import *
+
+def go_home():
+    title.place(relx = 0.5, y = 80, anchor = CENTER)
+    create_button.place(relx = 0.5, y = 140, anchor = CENTER)
+    reminders_button.place(relx = 0.5, y = 180, anchor = CENTER)
+    about_button.place(relx = 0.5, y = 220, anchor = CENTER)
+
+def leave_page(page_and_home):
+    page = page_and_home[0]
+    for i in range(0, len(page)):
+        page[i].place_forget()
+    if page_and_home[1] == True:
+        go_home()
+      
+def back_button(page_and_home):
+    back = Button(main, text = "<---", command = lambda: leave_page(page_and_home))
+    back.place(relx = 0.15, y = 350, anchor = CENTER)
+
+def create_page():
+    page_and_home = [home_page, False]
+    leave_page(page_and_home)
+    title = Label(main, text="Create a Reminder", font=(40))
+    title.place(relx = 0.5, y = 80, anchor = CENTER)
+    page = [title]
+    page_and_home = [page, True]
+    back_button(page_and_home)
+
+def reminders_page():
+    page_and_home = [home_page, False]
+    leave_page(page_and_home)
+    title = Label(main, text ="Reminders", font = (40))
+    title.place(relx = 0.5, y = 80, anchor = CENTER)
+    page = [title]
+    page_and_home = [page, True]
+    back_button(page_and_home)
+
+def about_page():
+    page_and_home = [home_page, False]
+    leave_page(page_and_home)
+    title = Label(main, text ="About", font = (40))
+    title.place(relx = 0.5, y = 80, anchor = CENTER)
+    page = [title]
+    page_and_home = [page, True]
+    back_button(page_and_home)
+
+#initialising the window, setting some properties and creating a canvas
+main = Tk()
+main.geometry("500x400")
+main.title("Reminder Program")
+
+#home page
+title = Label(main, text = "The Reminders App", font = (40))
+create_button = Button(main, text = "Create a Reminder", command = create_page)
+reminders_button = Button(main, text = "Reminders", command = reminders_page)
+about_button = Button(main, text = "About", command = about_page)
+
+#############################remmeber to this below when adding new things######################################################
+home_page = [title, create_button, reminders_button, about_button]
+
+go_home()
+
+#show window
+
+main.mainloop()
+
+'''
+#creating an entry box
+entry_box = Entry(main)
+canvas.create_window(200, 50, window=entry_box)
+
+'''
---
+++ trial.py
@@ -0,0 +1,30 @@
+import tkinter
+
+def page1():
+    page2text.pack_forget()
+    page1text.pack()
+    page2button.pack_forget()
+
+def page2():
+    page1text.pack_forget()
+    for i in range (0, len(array)):
+        array[i].pack()
+
+window = tkinter.Tk()
+
+page1btn = tkinter.Button(window, text="Page 1", command=page1)
+page2btn = tkinter.Button(window, text="Page 2", command=page2)
+
+page1text = tkinter.Label(window, text="This is page 1")
+
+
+page2text = tkinter.Label(window, text="This is page 2")
+page2button = tkinter.Button(window, text= "button")
+
+array = [page2text, page2button]
+
+page1btn.pack()
+page2btn.pack()
+page1text.pack()
+
+window.mainloop()
